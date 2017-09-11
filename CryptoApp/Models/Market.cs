aae7798ab6a1c0d249e6732b1641dd240fdb7273
IhniCC <frankowicz.marek@gmail.com>
2017-09-11 13:13:44 +0200

    Implement RatesSignatures enumeration

--- CryptoApp/Models/Market.cs
+++ CryptoApp/Models/Market.cs
@@ -7,4 +7,20 @@ namespace CryptoApp.Models
         private static readonly Lazy<Market> _instance =
             new Lazy<Market>(() => new Market());
     }
+
+    public enum RatesSignatures
+    {
+        EurEth,
+        EurBtc,
+        EurLtc,
+        EthEur,
+        BtcEur,
+        LtcEur,
+        EthBtc,
+        EthLtc,
+        BtcEth,
+        BtcLtc,
+        LtcEth,
+        LtcBtc
+    }
 }
