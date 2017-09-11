0f9cad3eb3d814c4263bb7e2933eefbddbea488a
IhniCC <frankowicz.marek@gmail.com>
2017-09-11 13:02:37 +0200

    Implement Marekt lazy initialization

--- CryptoApp/Models/Market.cs
+++ CryptoApp/Models/Market.cs
@@ -1,7 +1,10 @@
-﻿namespace CryptoApp.Models
+﻿using System;
+
+namespace CryptoApp.Models
 {
     public class Market
     {
-        
+        private static readonly Lazy<Market> _instance =
+            new Lazy<Market>(() => new Market());
     }
 }
