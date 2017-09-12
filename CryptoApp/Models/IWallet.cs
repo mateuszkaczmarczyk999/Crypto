4df390307af2aa09183caee2ae2613758e9a69bc
Michał G <m.k.gozdzikiewicz@gmail.com>
2017-09-12 14:01:33 +0200

    Create IWallet interface

---
+++ CryptoApp/Models/IWallet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+public interface IWallet
+{
+    bool IsEnoughFunds(CurrencySignatures toSell, decimal quantity);
+
+    bool SubstractFunds(CurrencySignatures toSell, decimal quantity);
+
+    bool AddFunds(CurrencySignatures toBuy, decimal quantity);
+}
