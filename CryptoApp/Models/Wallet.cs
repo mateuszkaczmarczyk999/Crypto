72696e9b5596d3173c7a2b528892a4abcba4786c
Michał G <m.k.gozdzikiewicz@gmail.com>
2017-09-12 14:04:02 +0200

    Update Wallet fields

--- CryptoApp/Models/Wallet.cs
+++ CryptoApp/Models/Wallet.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 
 public class Wallet
 {
     public string Id { get; set; }
-    public ArrayList<Currency> MyCurrencies { get; set; }
+    public Dictionary MyCurrencies { get; set; }
 	public Wallet()
 	{
 		
