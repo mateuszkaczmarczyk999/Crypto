40cf547f2dfc69a066a47b1b1bbb8a95e6d938e5
Michał G <m.k.gozdzikiewicz@gmail.com>
2017-09-13 10:00:40 +0200

    Create custom exception

---
+++ CryptoApp/Errors/NotEnoughFundsException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CryptoApp.Errors
+
+{
+    public class NotEnoughFundsException: Exception
+    {
+        public NotEnoughFundsException()
+        {
+        }
+
+        public NotEnoughFundsException(string message)
+            : base(message)
+        {
+        }
+
+        public NotEnoughFundsException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+       
+}
