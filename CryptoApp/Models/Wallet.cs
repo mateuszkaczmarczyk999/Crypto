b5c0b59dffd2ace02f099d78f3e75350c1c0db8e
Michał G <m.k.gozdzikiewicz@gmail.com>
2017-09-13 09:47:54 +0200

    Fix methods access modifiers

--- CryptoApp/Models/Wallet.cs
+++ CryptoApp/Models/Wallet.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CryptoApp.Models
 {
@@ -6,28 +7,27 @@ namespace CryptoApp.Models
     public class Wallet : IWallet
     {
         public string Id { get; set; }
-        public Dictionary<CurrencySignatures, decimal>  MyCurrencies { get; set; }
+        private Dictionary<CurrencySignatures, decimal>  MyCurrencies { get; set; }
 
         public Wallet()
         {
             
         }
 
-        bool HasEnoughFunds(CurrencySignatures toSell, decimal quantity)
-        {
-            
-                if (MyCurrencies[toSell] >= toSell)
+        public bool HasEnoughFunds(CurrencySignatures toSell, decimal quantity)
+        {          
+                if (MyCurrencies[toSell] >= quantity)
                 {
                     return true;
-                }
+            }
             return false;
         }
 
-        bool SubstractFunds(CurrencySignatures toSell, decimal quantity)
+        public bool SubstractFunds(CurrencySignatures toSell, decimal quantity)
         {
             try
             {
-                MyCurrencies[toSell] = Decimal.Substract(MyCurrencies[toSell], quantity);
+                MyCurrencies[toSell] = Decimal.Subtract(MyCurrencies[toSell], quantity);
                 return true;
             }
             catch
