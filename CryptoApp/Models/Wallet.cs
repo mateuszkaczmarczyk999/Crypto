0e21bf82e86a4ab374cc41a372dccc5b5db1149d
Michał G <m.k.gozdzikiewicz@gmail.com>
2017-09-13 10:33:54 +0200

    Reformat

--- CryptoApp/Models/Wallet.cs
+++ CryptoApp/Models/Wallet.cs
@@ -1,49 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using CryptoApp.Errors;
 
 namespace CryptoApp.Models
 {
-
     public class Wallet : IWallet
     {
         public string Id { get; set; }
-        private Dictionary<CurrencySignatures, decimal>  MyCurrencies { get; set; }
-
-        public Wallet()
-        {
-            
-        }
+        private Dictionary<CurrencySignatures, decimal> MyCurrencies { get; set; }
 
         public bool HasEnoughFunds(CurrencySignatures toSell, decimal quantity)
-        {          
-                if (MyCurrencies[toSell] >= quantity)
-                {
-                    return true;
-            }
+        {
+            if (MyCurrencies[toSell] >= quantity)
+                return true;
             return false;
         }
 
         public void SubstractFunds(CurrencySignatures toSell, decimal quantity)
         {
             if (HasEnoughFunds(toSell, quantity))
-            {
-                MyCurrencies[toSell] = Decimal.Subtract(MyCurrencies[toSell], quantity);
-            }
+                MyCurrencies[toSell] = decimal.Subtract(MyCurrencies[toSell], quantity);
             else
-            {
                 throw new NotEnoughFundsException();
-            }
-
-
-
         }
-        
+
         public void AddFunds(CurrencySignatures toBuy, decimal quantity)
         {
-            MyCurrencies[toBuy] = Decimal.Add(MyCurrencies[toBuy], quantity);
+            MyCurrencies[toBuy] = decimal.Add(MyCurrencies[toBuy], quantity);
         }
-
-
     }
 }
