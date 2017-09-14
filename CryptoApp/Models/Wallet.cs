489c9661560f0b9bdac195de60c01ff304e91d6b
Michał G <m.k.gozdzikiewicz@gmail.com>
2017-09-14 14:08:25 +0200

    Refactor funds Collection

--- CryptoApp/Models/Wallet.cs
+++ CryptoApp/Models/Wallet.cs
@@ -10,30 +10,30 @@ namespace CryptoApp.Models
     {
         public Wallet()
         {
-            MyCurrencies = new Dictionary<CurrenciesSignatures, decimal>();
+            MyFunds = new List<Currency>();
             foreach (var currencySignature in Enum.GetValues(typeof(CurrenciesSignatures)).Cast<CurrenciesSignatures>())
-                MyCurrencies.Add(currencySignature, 0);
+                MyFunds.Add(new Currency{CurrencySignature = currencySignature, Value = 0});
         }
 
         public string Id { get; set; }
-        public Dictionary<CurrenciesSignatures, decimal> MyCurrencies { get; }
+        public List<Currency> MyFunds { get; }
 
         public bool HasEnoughFunds(CurrenciesSignatures toSell, decimal quantity)
         {
-            return MyCurrencies[toSell] >= quantity;
+            return MyFunds[toSell] >= quantity;
         }
 
         public void SubstractFunds(CurrenciesSignatures toSell, decimal quantity)
         {
             if (HasEnoughFunds(toSell, quantity))
-                MyCurrencies[toSell] = decimal.Subtract(MyCurrencies[toSell], quantity);
+                MyFunds[toSell] = decimal.Subtract(MyFunds[toSell], quantity);
             else
                 throw new NotEnoughFundsException();
         }
 
         public void AddFunds(CurrenciesSignatures toBuy, decimal quantity)
         {
-            MyCurrencies[toBuy] = decimal.Add(MyCurrencies[toBuy], quantity);
+            MyFunds[toBuy] = decimal.Add(MyFunds[toBuy], quantity);
         }
     }
 }
