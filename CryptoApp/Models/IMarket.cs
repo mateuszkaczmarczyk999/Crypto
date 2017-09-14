5f0821375e9c780835e7611e0557f62e6e32f61e
IhniCC <frankowicz.marek@gmail.com>
2017-09-14 12:29:41 +0200

    Add IMarket interface

---
+++ CryptoApp/Models/IMarket.cs
@@ -0,0 +1,10 @@
+﻿using CryptoApp.Enums;
+
+namespace CryptoApp.Models
+{
+    public interface IMarket
+    {
+        bool Exchange(CurrenciesSignatures toSell, CurrenciesSignatures toBuy, decimal quantity, IWallet wallet);
+        void OnRatesUpdated(object source, CurrenciesRatesEvantArgs args);
+    }
+}
