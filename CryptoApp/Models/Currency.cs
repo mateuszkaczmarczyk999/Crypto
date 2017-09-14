b0c66714db9bf8e7efda2740a6aec5d2e2c4e353
Michał G <m.k.gozdzikiewicz@gmail.com>
2017-09-14 14:07:22 +0200

    Create currency model

---
+++ CryptoApp/Models/Currency.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using CryptoApp.Enums;
+
+namespace CryptoApp.Models
+{
+    public class Currency
+    {
+        public string Id { get; set; }
+        
+        [Required]
+        public CurrenciesSignatures CurrencySignature { get; set; }
+        
+        [Required]
+        public decimal Value { get; set; }
+        
+        
+    }
+}
