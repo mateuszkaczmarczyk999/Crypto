72078d6e3102f0b3a1fe8cd0f2003687e07ab183
Michał G <m.k.gozdzikiewicz@gmail.com>
2017-09-15 09:53:23 +0200

    Create extension for Identity

---
+++ CryptoApp/Extensions/IdentityExtensions.cs
@@ -0,0 +1,13 @@
+﻿
+namespace CryptoApp.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static string GetOrganizationId(this System.Security.Principal.IIdentity identity)
+        {
+            var claim = ((System.Security.Claims.ClaimsIdentity)identity).FindFirst("WalletId");
+
+            return (claim != null) ? claim.Value : string.Empty;
+        }
+    }
+}
