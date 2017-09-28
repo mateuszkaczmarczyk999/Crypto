308b691891aef87a08cc9749d9cedb19fb452719
Michał Kobiec <mkkc12@gmail.com>
2017-09-28 17:02:04 +0200

    Move rateprovider from controller to app_start and init it in global.asax

---
+++ CryptoApp/App_Start/RatesProviderConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using CryptoApp.Hubs;
+using CryptoRatesProvider;
+using CryptoRatesProvider.Enums;
+
+namespace CryptoApp
+{
+    public class RatesProviderConfig
+    {
+        public static void RegisterProvider()
+        {
+            var providerFactory = new RatesProviderServiceFactory(
+                new RatePair(CurrencySignature.Btc, CurrencySignature.Eur),
+                new RatePair(CurrencySignature.Eth, CurrencySignature.Eur),
+                new RatePair(CurrencySignature.Ltc, CurrencySignature.Eur),
+                new RatePair(CurrencySignature.Eth, CurrencySignature.Btc),
+                new RatePair(CurrencySignature.Eth, CurrencySignature.Ltc),
+                new RatePair(CurrencySignature.Btc, CurrencySignature.Ltc));
+
+            var provider = providerFactory.GerProvider(RateProvider.CryptoCompare);
+            provider.RatesUpdated += (sender, eventArgs) => TestMarket.GetInstance().OnRatesUpdated(sender, eventArgs);
+            provider.StartService();
+
+            //            provider.RatesUpdated += (sender, eventArgs) =>
+            //            {
+            //                Debug.WriteLine(
+            //                    $"from {eventArgs.ChangeFrom} to {eventArgs.ChangeTo} {eventArgs.PriceAction} {eventArgs.Value}"
+            //                );
+            //            };
+        }
+    }
+}
--- CryptoApp/Controllers/MarketController.cs
+++ CryptoApp/Controllers/MarketController.cs
@@ -18,23 +18,6 @@ namespace CryptoApp.Controllers
         // GET: Market
         public ActionResult Index()
         {
-            var providerFactory = new RatesProviderServiceFactory(
-                new RatePair(CurrencySignature.Btc, CurrencySignature.Eur),
-                new RatePair(CurrencySignature.Eth, CurrencySignature.Eur),
-                new RatePair(CurrencySignature.Ltc, CurrencySignature.Eur),
-                new RatePair(CurrencySignature.Eth, CurrencySignature.Btc),
-                new RatePair(CurrencySignature.Eth, CurrencySignature.Ltc),
-                new RatePair(CurrencySignature.Btc, CurrencySignature.Ltc));
-
-            var provider = providerFactory.GerProvider(RateProvider.CryptoCompare);
-            provider.RatesUpdated += (sender, eventArgs) =>
-            {
-                Debug.WriteLine(
-                    $"from {eventArgs.ChangeFrom} to {eventArgs.ChangeTo} {eventArgs.PriceAction} {eventArgs.Value}"
-                );
-            };
-            provider.RatesUpdated += (sender, eventArgs) => TestMarket.GetInstance().OnRatesUpdated(sender, eventArgs);
-            provider.StartService();
             var curencyCollection = Enum.GetValues(typeof(CurrencySignature));
             return View(curencyCollection);
         }
